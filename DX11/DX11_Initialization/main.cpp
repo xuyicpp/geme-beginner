0c9642838a353d73623307aac135f1b47b2295c0
xuyi <540767457@qq.com>
2019-01-14 19:19:55 +0800

    add

---
+++ DX11/DX11_Initialization/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <Windows.h>
+//#include <d3dx11.h>
+#include <d3d11.h>                                     
+//发
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    MessageBox(nullptr, L"喵", L"喵", 0);
+    return 0;
+}
