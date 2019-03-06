41104e7e516bf23e331cd8a801df348a4d8c1a0e
小徐 <yixu_cs@hust.edu.cn>
2019-03-06 16:34:21 +0800

    revise ch2

---
+++ Unity3D入门_2018_3_3版本/ch03_AddEnemyAndBullet/Assets/FPSInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]     //强制依赖其他组件
+[AddComponentMenu("Control Script/FPS Input")]      //添加到添加菜单(Add Component)
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    private CharacterController _charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); //限制向量长度
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+
+    }
+}
---
+++ Unity3D入门_2018_3_3版本/ch03_AddEnemyAndBullet/Assets/RayShooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();       //访问相同对象上附加的其他组件
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);    //屏幕中心是宽高的一半
+            Ray ray = _camera.ScreenPointToRay(point);  //在摄像机所在位置创建射线
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))       //Raycast 给引用的变量填充信息
+            {
+                Debug.Log("Hit " + hit.point);
+            }
+        }
+    }
+}
